89cf861a69a68a6e10f823e0619dece71ef6dc0a
Hasan Karahan <hasan.karahan@blackhan.com>
2015-11-19 22:19:59 +0400

    XHR: attempting sync approach

--- example/server/py/rpc-server.py
+++ example/server/py/rpc-server.py
@@ -2,6 +2,7 @@
 ###############################################################################
 
 import argparse, os
+
 import tornado.web
 import tornado.websocket
 import tornado.ioloop
@@ -16,54 +17,59 @@ from protocol import calculator_pb2 as Calculator
 ###############################################################################
 ###############################################################################
 
+def process(data):
+    rpc_req = Rpc.Request()
+    rpc_req.ParseFromString(data)
+
+    if rpc_req.name == '.Reflector.Service.ack':
+        req = Reflector.AckRequest()
+        req.ParseFromString(rpc_req.data)
+        res = Reflector.AckResult()
+        res.timestamp = req.timestamp
+
+    elif rpc_req.name == '.Calculator.Service.add':
+        req = Calculator.AddRequest()
+        req.ParseFromString(rpc_req.data)
+        res = Calculator.AddResult()
+        res.value = req.lhs + req.rhs
+
+    elif rpc_req.name == '.Calculator.Service.sub':
+        req = Calculator.SubRequest()
+        req.ParseFromString(rpc_req.data)
+        res = Calculator.SubResult()
+        res.value = req.lhs - req.rhs
+
+    elif rpc_req.name == '.Calculator.Service.mul':
+        req = Calculator.MulRequest()
+        req.ParseFromString(rpc_req.data)
+        res = Calculator.MulResult()
+        res.value = req.lhs * req.rhs
+
+    elif rpc_req.name == '.Calculator.Service.div':
+        req = Calculator.DivRequest()
+        req.ParseFromString(rpc_req.data)
+        res = Calculator.DivResult()
+        res.value = req.lhs / req.rhs
+
+    else:
+        raise Exception('{0}: not supported'.format(rpc_req.name))
+
+    rpc_res = Rpc.Response()
+    rpc_res.data = res.SerializeToString()
+    rpc_res.id = rpc_req.id
+
+    return rpc_res.SerializeToString()
+
+###############################################################################
+###############################################################################
+
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
     def on_message(self, data):
-
         if arguments.logging:
-            print '[on:message]', repr(data)
+            print '[ws:message]', repr(data), type(data)
 
-        rpc_req = Rpc.Request()
-        rpc_req.ParseFromString(data)
-
-        if rpc_req.name == '.Reflector.Service.ack':
-            req = Reflector.AckRequest()
-            req.ParseFromString(rpc_req.data)
-            res = Reflector.AckResult()
-            res.timestamp = req.timestamp
-
-        elif rpc_req.name == '.Calculator.Service.add':
-            req = Calculator.AddRequest()
-            req.ParseFromString(rpc_req.data)
-            res = Calculator.AddResult()
-            res.value = req.lhs + req.rhs
-
-        elif rpc_req.name == '.Calculator.Service.sub':
-            req = Calculator.SubRequest()
-            req.ParseFromString(rpc_req.data)
-            res = Calculator.SubResult()
-            res.value = req.lhs - req.rhs
-
-        elif rpc_req.name == '.Calculator.Service.mul':
-            req = Calculator.MulRequest()
-            req.ParseFromString(rpc_req.data)
-            res = Calculator.MulResult()
-            res.value = req.lhs * req.rhs
-
-        elif rpc_req.name == '.Calculator.Service.div':
-            req = Calculator.DivRequest()
-            req.ParseFromString(rpc_req.data)
-            res = Calculator.DivResult()
-            res.value = req.lhs / req.rhs
-
-        else:
-            raise Exception('{0}: not supported'.format(rpc_req.name))
-
-        rpc_res = Rpc.Response()
-        rpc_res.data = res.SerializeToString()
-        rpc_res.id = rpc_req.id
-
-        self.write_message(rpc_res.SerializeToString(), binary=True)
+        self.write_message(process(data), binary=True)
 
     def check_origin(self, origin):
 
@@ -71,7 +77,19 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
 ###############################################################################
 
-application = tornado.web.Application([(r'/', WebSocketHandler)])
+class XhrHandler(tornado.web.RequestHandler):
+
+    def post(self):
+        if arguments.logging:
+            print '[xhr:post]', self.request.body
+
+        self.write(process(self.request.body))
+
+###############################################################################
+
+application = tornado.web.Application([
+    (r'/', WebSocketHandler), (r'/xhr', XhrHandler)
+])
 
 ###############################################################################
 ###############################################################################
