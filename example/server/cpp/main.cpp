30aa3e10a013e0228caeb0e7e6520113269f9dc0
Hasan Karahan <hasan.karahan@blackhan.com>
2015-11-30 01:44:39 +0400

    Server.cpp: minor

--- example/server/cpp/main.cpp
+++ example/server/cpp/main.cpp
@@ -17,11 +17,11 @@ int main(int argc, char *argv[]) {
                 QStringList() << "l" << "logging",
                 QCoreApplication::translate("main", "Logging [default: false]"));
     parser.addOption(logging_opt);
-    QCommandLineOption tcp_port_opt(
-                QStringList() << "tcp-port",
-                QCoreApplication::translate("main", "TCP Server Port [default: 8088]"),
-                QCoreApplication::translate("main", "tcp-port"), QStringLiteral("8088"));
-    parser.addOption(tcp_port_opt);
+    QCommandLineOption xhr_port_opt(
+                QStringList() << "xhr-port",
+                QCoreApplication::translate("main", "XHR Server Port [default: 8088]"),
+                QCoreApplication::translate("main", "xhr-port"), QStringLiteral("8088"));
+    parser.addOption(xhr_port_opt);
     QCommandLineOption ws_port_opt(
                 QStringList() << "ws-port",
                 QCoreApplication::translate("main", "WS Server Port [default: 8089]"),
@@ -31,15 +31,13 @@ int main(int argc, char *argv[]) {
 
     bool logging = parser.isSet(logging_opt);
     Q_ASSERT(logging == true || logging == false);
-    int port_tcp = parser.value(tcp_port_opt).toInt();
-    Q_ASSERT(port_tcp);
+    int port_xhr = parser.value(xhr_port_opt).toInt();
+    Q_ASSERT(port_xhr);
     int port_ws = parser.value(ws_port_opt).toInt();
     Q_ASSERT(port_ws);
 
-    RpcServer *server = new RpcServer(port_tcp, port_ws);
-    Q_ASSERT(server);
+    RpcServer *server = new RpcServer(port_xhr, port_ws);
     server->setLogging(logging);
-    Q_ASSERT(server->getLogging() == logging);
 
     QObject::connect(server, &RpcServer::closed, &app, &QCoreApplication::quit);
     return app.exec();
